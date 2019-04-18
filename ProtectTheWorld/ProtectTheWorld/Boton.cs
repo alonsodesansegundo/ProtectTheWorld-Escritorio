a75b268c04b74e0f7eb2bbc09f6d04f6bb8e9676
alonsodesansegundo <alonsodesansegundo@gmail.com>
2019-04-18 18:30:37 +0200

    Método setImagen en clase boton (sin probar)

--- ProtectTheWorld/ProtectTheWorld/Boton.cs
+++ ProtectTheWorld/ProtectTheWorld/Boton.cs
@@ -15,17 +15,25 @@ namespace ProtectTheWorld
         private Texture2D rectangulo;
         private Color[] data;
         private Vector2 punto;
+        private int x,y,ancho, alto;
         //Para el texto del boton
         private string texto;
         private SpriteFont fuente;
-        private Color color;
-        public Boton(GraphicsDeviceManager graphics, SpriteBatch spriteBatch,int x, int y, int ancho, int alto,Color color)
+        private Color colorTexto;
+        //Imagen boton
+        private Texture2D imagen;
+        public Boton(GraphicsDeviceManager graphics, SpriteBatch spriteBatch, int x, int y, int ancho, int alto, Color color)
+      
         {
             this.graphics = graphics;
             this.spriteBatch = spriteBatch;
-            rectangulo=new Texture2D(graphics.GraphicsDevice, ancho, alto);
+            this.x = x;
+            this.y = y;
+            this.ancho = ancho;
+            this.alto = alto;
+            rectangulo = new Texture2D(graphics.GraphicsDevice, ancho, alto);
             data = new Color[ancho * alto];
-            for (int i = 0; i < data.Length; ++i) data[i] =color;
+            for (int i = 0; i < data.Length; ++i) data[i] = color;
             rectangulo.SetData(data);
             punto = new Vector2(x, y);
         }
@@ -34,14 +42,24 @@ namespace ProtectTheWorld
             spriteBatch.Draw(rectangulo, punto, Color.White);
             if (texto != null)
             {
-                spriteBatch.DrawString(fuente, texto,punto, color);
+                spriteBatch.DrawString(fuente, texto, punto, colorTexto);
+            }
+            if (imagen != null)
+            {
+                spriteBatch.Draw(imagen, new Rectangle(x, y,
+               ancho, alto), Color.White);
             }
         }
         public void SetTexto(string texto, SpriteFont fuente, Color color)
         {
             this.texto = texto;
             this.fuente = fuente;
-            this.color = color;
+            this.colorTexto = color;
+        }
+        public void SetImagen(Texture2D imagen)
+        {
+            this.imagen = imagen;
+
         }
     }
 }
